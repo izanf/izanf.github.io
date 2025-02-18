86c955eada931c2f8a49a063f92554a3d81319bc
Izanderson Florencio <izandersonf@gmail.com>
2025-02-18 17:20:16 -0300

    fix: remove navigator

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,5 @@
-const language = navigator?.language ?? 'en-US';
-
 export const formatDate = (date: string) => {
   const options = { year: 'numeric' as const, month: 'long' as const, day: 'numeric' as const };
 
-  return Intl.DateTimeFormat(language, options).format(new Date(date));
+  return Intl.DateTimeFormat('en-US', options).format(new Date(date));
 }
