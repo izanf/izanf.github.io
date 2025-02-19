e00f811f764200da4b540f5df29fd0a190a0c574
Izanderson Florencio <izandersonf@gmail.com>
2025-02-19 14:59:33 -0300

    feat: no data found component

--- src/components/NoData.tsx
+++ src/components/NoData.tsx
@@ -2,6 +2,12 @@ import Icon from './Icon';
 
 export default function NoData() {
   return (
-    <Icon name="faBars" />
+    <div className="flex items-center flex-col items-center py-12">
+      <div>
+        <Icon name="faDatabase" className="text-3xl" />
+        <Icon name="faSlash" className="text-3xl ml-[-32]" />
+      </div>
+      <span className="p-2">No data found</span>
+    </div>
   )
 }
